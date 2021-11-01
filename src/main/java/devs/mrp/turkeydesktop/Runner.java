085017abb8bc97bc63e1c6898ff08f89b2583852
miguel <48907268+zemua@users.noreply.github.com>
2021-11-01 21:50:43 +0100

    first netbeans commit

---
+++ src/main/java/devs/mrp/turkeydesktop/Runner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devs.mrp.turkeydesktop;
+
+/**
+ *
+ * @author miguel
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("my turkey desktop runner initialized");
+    }
+}
