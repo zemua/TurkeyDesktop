a378fab346edde380232792e53edee30052862ca
miguel <48907268+zemua@users.noreply.github.com>
2021-11-03 19:05:34 +0100

    setting things up

---
+++ src/main/java/devs/mrp/turkeydesktop/controllers/MainController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devs.mrp.turkeydesktop.controllers;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author miguel
+ */
+public class MainController implements IStarter {
+
+    private JFrame mainFrame;
+    
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
