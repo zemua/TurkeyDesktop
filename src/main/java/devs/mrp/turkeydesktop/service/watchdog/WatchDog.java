61204cc9912fab03d98c5dad81dbf77312a12f5c
miguel <48907268+zemua@users.noreply.github.com>
2021-11-07 17:19:05 +0100

    watchdog & logger

--- src/main/java/devs/mrp/turkeydesktop/service/watchdog/WatchDog.java
+++ src/main/java/devs/mrp/turkeydesktop/service/watchdog/WatchDog.java
@@ -5,10 +5,8 @@
  */
 package devs.mrp.turkeydesktop.service.watchdog;
 
-import java.util.concurrent.ExecutionException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JTextArea;
 import javax.swing.SwingWorker;
 
@@ -17,24 +15,26 @@ import javax.swing.SwingWorker;
  * @author miguel
  */
 public class WatchDog implements IWatchDog {
-    
+
     private static final long SLEEP_MILIS = 3000;
-    
+
     private boolean on = true;
     private JTextArea mLogger;
     private SwingWorker worker;
     private AtomicLong timestamp;
-    
+
     public WatchDog() {
-        initializeWorker();
     }
 
     @Override
     public void begin(JTextArea logger) {
-        if (!worker.getState().equals(SwingWorker.StateValue.STARTED) &&
-                (worker.isCancelled() || worker.isDone() || worker.getState().equals(SwingWorker.StateValue.PENDING))) {
+        if (worker == null || worker.isDone() || worker.getState().equals(SwingWorker.StateValue.PENDING)) {
+            initializeWorker();
+            setLogger(logger);
+            timestamp = new AtomicLong(System.currentTimeMillis());
             worker.execute();
         }
+
     }
 
     @Override
@@ -46,28 +46,33 @@ public class WatchDog implements IWatchDog {
     public void stop() {
         on = false;
     }
-    
+
     private void log(String text) {
         mLogger.append(String.format("%s \n", text));
     }
-    
+
     private void initializeWorker() {
         this.worker = new SwingWorker() {
             @Override
             protected Object doInBackground() throws Exception {
-                while(WatchDog.this.on) {
+                while (WatchDog.this.on) {
                     Thread.sleep(SLEEP_MILIS);
-                    doLoopedStuff();
+                    publish();
                 }
                 return null;
             }
+
+            @Override
+            protected void process(List chunks) {
+                doLoopedStuff();
+            }
         };
     }
-    
+
     private void doLoopedStuff() {
         Long current = System.currentTimeMillis();
-        Long elapsed = current-timestamp.getAndSet(current);
-        log (String.format("elapsed %d millis", elapsed));
+        Long elapsed = current - timestamp.getAndSet(current);
+        log(String.format("elapsed %d millis", elapsed));
     }
 
 }
