5a5f344c3f032160252ce708a0401021d1f732cf
miguel <48907268+zemua@users.noreply.github.com>
2021-11-07 19:09:53 +0100

    Window process chain handler

---
+++ src/main/java/devs/mrp/turkeydesktop/service/processchecker/ProcessChecker.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devs.mrp.turkeydesktop.service.processchecker;
+
+/**
+ *
+ * @author miguel
+ */
+public class ProcessChecker implements IProcessChecker {
+
+    @Override
+    public String currentProcessName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String currentWindowTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
