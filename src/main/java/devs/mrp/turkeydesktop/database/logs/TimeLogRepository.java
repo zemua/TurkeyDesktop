073bbca5e58a803bd2482ac7e860fb9b81b5ce5d
miguel <48907268+zemua@users.noreply.github.com>
2021-11-22 22:28:11 +0100

    log repo

--- src/main/java/devs/mrp/turkeydesktop/database/logs/TimeLogRepository.java
+++ src/main/java/devs/mrp/turkeydesktop/database/logs/TimeLogRepository.java
@@ -34,6 +34,11 @@ public class TimeLogRepository implements TimeLogDao {
             stm.setString(3, element.getPid());
             stm.setString(4, element.getProcessName());
             stm.setString(5, element.getWindowTitle());
+            stm.executeUpdate();
+            ResultSet generatedId = stm.getGeneratedKeys();
+            if (generatedId.next()) {
+                return generatedId.getLong(1);
+            }
             return -1;
         } catch (SQLException ex) {
             logger.log(Level.SEVERE, null, ex);
@@ -43,12 +48,30 @@ public class TimeLogRepository implements TimeLogDao {
 
     @Override
     public long update(TimeLog element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement stm;
+        try {
+            stm = dbInstance.getConnection().prepareStatement("UPDATE WATCHDOG_LOG SET EPOCH=?, ELAPSED=?, PID=?, PROCESS_NAME=?, WINDOW_TITLE=? WHERE ID=?");
+            stm.setLong(1, element.getEpoch());
+            stm.setLong(2, element.getElapsed());
+            stm.setString(3, element.getPid());
+            stm.setString(4, element.getProcessName());
+            stm.setString(5, element.getWindowTitle());
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        return -1;
     }
 
     @Override
     public ResultSet findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement stm;
+        ResultSet rs;
+        try {
+            // get from last 24 hours only by default to not overload memory
+            long frame = System.currentTimeMillis() - (24*60*60*1000);
+            stm = dbInstance.getConnection().prepareStatement("SELECT * FROM WATCHDOG_LOG WHERE EPOCH>?");
+            
+        }
     }
 
     @Override
