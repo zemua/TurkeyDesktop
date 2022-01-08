71d83fec492a29e2b49549913e51a239cb112579
miguel <48907268+zemua@users.noreply.github.com>
2022-01-08 22:07:41 +0100

    implement categorize titles

--- src/main/java/devs/mrp/turkeydesktop/database/titledlog/TitledLogServiceFacade.java
+++ src/main/java/devs/mrp/turkeydesktop/database/titledlog/TitledLogServiceFacade.java
@@ -5,12 +5,16 @@
  */
 package devs.mrp.turkeydesktop.database.titledlog;
 
+import devs.mrp.turkeydesktop.common.Dupla;
 import devs.mrp.turkeydesktop.database.logs.FTimeLogService;
 import devs.mrp.turkeydesktop.database.logs.ITimeLogService;
 import devs.mrp.turkeydesktop.database.titles.FTitleService;
 import devs.mrp.turkeydesktop.database.titles.ITitleService;
+import devs.mrp.turkeydesktop.database.titles.Title;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -23,7 +27,17 @@ public class TitledLogServiceFacade implements ITitledLogServiceFacade {
 
     @Override
     public List<TitledLog> getLogsWithTitleConditions(Date from, Date to) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return logService.logsGroupedByTitle(from, to).stream()
+                .map(e -> {
+                    TitledLog tl = new TitledLog();
+                    tl.setTitle(e.getValue1());
+                    tl.setElapsed(e.getValue2());
+                    tl.setConditions(titleService.findContainedBy(e.getValue1()));
+                    tl.setQtyPositives(titleService.countTypesOf(Title.Type.POSITIVE, e.getValue1()));
+                    tl.setQtyNegatives(titleService.countTypesOf(Title.Type.NEGATIVE, e.getValue1()));
+                    return tl;
+                })
+                .collect(Collectors.toList());
     }
     
 }
