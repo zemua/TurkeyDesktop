fcc905fa416226948b7bd4fe33032af7f0bdacfb
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-04-09 13:50:35 +0200

    implement killer in mac

--- src/main/java/devs/mrp/turkeydesktop/service/processkiller/KillerChainCommander.java
+++ src/main/java/devs/mrp/turkeydesktop/service/processkiller/KillerChainCommander.java
@@ -15,10 +15,14 @@ import devs.mrp.turkeydesktop.common.ChainHandler;
 public class KillerChainCommander implements ChainCommander {
     
     private ChainHandler<String> linuxHandler;
+    private ChainHandler<String> macosHandler;
     
     @Override
     public ChainHandler<String> getHandlerChain() {
         linuxHandler = new KillerChainHandlerLinuxBash();
+        macosHandler = new KillerChainHandlerMacos();
+        
+        linuxHandler.setNextHandler(macosHandler);
         
         return linuxHandler;
     }
