343496aa1e7846c6870fa7af6ce3b69bece63f9f
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-05-22 20:31:43 +0200

    remove old log entries on app start

--- src/main/java/devs/mrp/turkeydesktop/database/Db.java
+++ src/main/java/devs/mrp/turkeydesktop/database/Db.java
@@ -5,6 +5,7 @@
  */
 package devs.mrp.turkeydesktop.database;
 
+import devs.mrp.turkeydesktop.common.TimeConverter;
 import devs.mrp.turkeydesktop.database.closeables.Closeable;
 import devs.mrp.turkeydesktop.database.conditions.Condition;
 import devs.mrp.turkeydesktop.database.group.Group;
@@ -125,7 +126,7 @@ public class Db { // TODO create asynchronous listeners to update livedata
                 + "PRIMARY KEY (%s))",
                 WATCHDOG_TABLE, TimeLog.ID, TimeLog.EPOCH, TimeLog.ELAPSED, TimeLog.COUNTED, TimeLog.ACCUMULATED, TimeLog.PID, TimeLog.PROCESS_NAME, TimeLog.WINDOW_TITLE, Group.GROUP, Type.TYPE, TimeLog.ID));
         
-        execute(String.format("CREATE INDEX IF NOT EXISTS %s ON %s(%s)",
+        execute(String.format("CREATE UNIQUE INDEX IF NOT EXISTS %s ON %s(%s)",
                 "EPOCH_INDEX", WATCHDOG_TABLE, TimeLog.EPOCH));
 
         execute(String.format("CREATE TABLE IF NOT EXISTS %s("
@@ -193,6 +194,12 @@ public class Db { // TODO create asynchronous listeners to update livedata
                 + "%s VARCHAR(50), " // process name
                 + "PRIMARY KEY (%s))",
                 CLOSEABLES_TABLE, Closeable.PROCESS_NAME, Closeable.PROCESS_NAME));
+        
+        // REMOVE OLD LOG ENTRIES THAT ARE OF NO USE
+        execute(String.format("DELETE FROM %s WHERE %s < %s",
+                WATCHDOG_TABLE,
+                TimeLog.EPOCH,
+                TimeConverter.beginningOfOffsetDays(30)));
         
         //close();
     }
