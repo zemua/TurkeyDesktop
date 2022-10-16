daf23f1561b1dbfe38667bd1c00add53868573ac
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-10-16 13:55:21 +0200

    refactoring to do db work in the background

--- src/main/java/devs/mrp/turkeydesktop/database/group/GroupServiceFactory.java
+++ src/main/java/devs/mrp/turkeydesktop/database/group/GroupServiceFactory.java
@@ -5,6 +5,11 @@
  */
 package devs.mrp.turkeydesktop.database.group;
 
+import devs.mrp.turkeydesktop.common.GenericWorker;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  *
  * @author miguel
@@ -13,6 +18,14 @@ public class GroupServiceFactory {
     
     public static GroupService getService() {
         return new GroupServiceImpl();
+    }
+    
+    public static void runGroupWorker(Supplier<Group> supplier, Consumer<Group> consumer) {
+        new GenericWorker<Group>().runWorker(supplier, consumer);
+    }
+    
+    public static void runGroupListWorker(Supplier<List<Group>> supplier, Consumer<List<Group>> consumer) {
+        new GenericWorker<List<Group>>().runWorker(supplier, consumer);
     }
     
 }
