6ea5e6379b0259b277661e8fb73183c4fdb57745
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-01-05 02:05:59 +0100

    testing

--- src/main/java/devs/mrp/turkeydesktop/common/impl/DbCacheImpl.java
+++ src/main/java/devs/mrp/turkeydesktop/common/impl/DbCacheImpl.java
@@ -2,6 +2,7 @@ package devs.mrp.turkeydesktop.common.impl;
 
 import devs.mrp.turkeydesktop.common.DbCache;
 import devs.mrp.turkeydesktop.common.SaveAction;
+import devs.mrp.turkeydesktop.database.TurkeyDbException;
 import devs.mrp.turkeydesktop.database.GeneralDao;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Observable;
@@ -11,48 +12,58 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DbCacheImpl<KEY, VALUE> implements DbCache<KEY, VALUE> {
     
     private Map<KEY, VALUE> cacheMap = Collections.synchronizedMap(new CacheMap<>(500));
     private GeneralDao<VALUE, KEY> repo;
     private Function<VALUE,KEY> keyExtractor;
     private Function<KEY,Boolean> isValidKey;
-    private Function<ResultSet,Observable<VALUE>> listFromResultSet;
+    private Function<ResultSet,Observable<VALUE>> streamFromResultSet;
     
     public DbCacheImpl(GeneralDao<VALUE, KEY> repo,
             Function<VALUE,KEY> keyExtractor,
             Function<KEY,Boolean> isValidKey,
-            Function<ResultSet,Observable<VALUE>> listFromResultSet) {
+            Function<ResultSet,Observable<VALUE>> streamFromResultSet) {
         this.repo = repo;
         this.keyExtractor = keyExtractor;
         this.isValidKey = isValidKey;
-        this.listFromResultSet = listFromResultSet;
+        this.streamFromResultSet = streamFromResultSet;
         loadDb();
     }
 
     @Override
     public Single<SaveAction> save(VALUE value) {
         KEY key = keyExtractor.apply(value);
-        Maybe<KEY> result = addOrUpdate(key, value)
-                .doOnSuccess(id -> cacheMap.put(id, value));
-        return whichSaveAction(result);
+        Single<KeyAndAction> updateOutput = addOrUpdate(key, value)
+            .doOnSuccess(keyAndAction -> cacheMap.put(keyAndAction.key, value));
+        return updateOutput.map(output -> output.action);
     }
     
-    private Single<SaveAction> whichSaveAction(Maybe<KEY> repoResult) {
-        return repoResult.isEmpty().map(isEmpty -> isEmpty ? SaveAction.SAVED : SaveAction.EXISTING);
-    }
-    
-    private Maybe<KEY> addOrUpdate(KEY key, VALUE value) {
-        Maybe<KEY> result;
+    private Single<KeyAndAction> addOrUpdate(KEY key, VALUE value) throws TurkeyDbException {
+        Single<KeyAndAction> result;
         if (canAddNew(key)) {
-            result = Maybe.fromSingle(repo.add(value));
+            result = repo.add(value).map(id -> new KeyAndAction(id, SaveAction.SAVED));
         } else if (canUpdate(key, value)) {
-            result = Maybe.fromSingle(repo.update(value).map(qty -> key));
+            result = repo.update(value).map(qty -> new KeyAndAction(key, SaveAction.UPDATED));
+        } else if (existing(key, value)) {
+            result = Single.just(new KeyAndAction(key, SaveAction.EXISTING));
         } else {
-            result = Maybe.empty();
+            log.error("Neither canAddNew, canUpdate, nor existing");
+            throw new TurkeyDbException();
         }
         return result;
+    }
+    
+    private class KeyAndAction {
+        KEY key;
+        SaveAction action;
+        KeyAndAction(KEY k, SaveAction a) {
+            key = k;
+            action = a;
+        }
     }
     
     private boolean canAddNew(KEY key) {
@@ -61,6 +72,10 @@ public class DbCacheImpl<KEY, VALUE> implements DbCache<KEY, VALUE> {
     
     private boolean canUpdate(KEY key, VALUE value) {
         return isValidKey.apply(key) && cacheMap.containsKey(key) && !cacheMap.get(key).equals(value);
+    }
+    
+    private boolean existing(KEY key, VALUE value) {
+        return isValidKey.apply(key) && cacheMap.containsKey(key) && cacheMap.get(key).equals(value);
     }
 
     @Override
@@ -92,7 +107,7 @@ public class DbCacheImpl<KEY, VALUE> implements DbCache<KEY, VALUE> {
     
     private void loadDb() {
         repo.findAll()
-                .flatMapObservable(listFromResultSet::apply)
+                .flatMapObservable(streamFromResultSet::apply)
                 .doOnNext(value -> cacheMap.put(keyExtractor.apply(value), value))
                 .subscribe();
     }
