5b0f0ce9ee6b1e1d9dfcd8526f2da56d559c1e86
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-01-06 18:16:40 +0100

    testing

---
+++ src/test/java/devs/mrp/turkeydesktop/database/closeables/CloseableServiceImplTest.java
@@ -0,0 +1,13 @@
+package devs.mrp.turkeydesktop.database.closeables;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class CloseableServiceImplTest {
+
+    @Test
+    public void testSomeMethod() {
+        fail();
+    }
+    
+}
