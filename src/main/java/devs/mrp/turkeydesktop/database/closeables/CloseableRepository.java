4bbfdb33b43a0c03575d6db110c095f2058f478f
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-01-06 18:15:26 +0100

    testing

--- src/main/java/devs/mrp/turkeydesktop/database/closeables/CloseableRepository.java
+++ src/main/java/devs/mrp/turkeydesktop/database/closeables/CloseableRepository.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package devs.mrp.turkeydesktop.database.closeables;
 
 import devs.mrp.turkeydesktop.database.Db;
+import devs.mrp.turkeydesktop.database.DbFactory;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import io.reactivex.rxjava3.core.Single;
+import lombok.extern.slf4j.Slf4j;
 
-/**
- *
- * @author miguel
- */
+@Slf4j
 public class CloseableRepository implements CloseableDao {
     
-    private Db dbInstance = Db.getInstance();
-    private Logger logger = Logger.getLogger(CloseableRepository.class.getName());
-    
     private static CloseableRepository instance;
+    
+    private Db dbInstance = DbFactory.getDb();
     
     private CloseableRepository() {}
     
@@ -35,30 +26,37 @@ public class CloseableRepository implements CloseableDao {
     
     @Override
     public Single<String> add(Closeable element) {
-        return Db.singleString(()-> {
-            PreparedStatement stm;
-            String result = "";
-            try {
-                stm = dbInstance.getConnection().prepareStatement(String.format("INSERT INTO %s (%s) ",
-                        Db.CLOSEABLES_TABLE, Closeable.PROCESS_NAME)
-                        + "VALUES (?)");
-                        // we don't retrieve generated keys as no keys are generated, we provide them
-                stm.setString(1, element.getProcess());
-                stm.executeUpdate();
-                result = element.getProcess();
-            } catch (SQLException ex) {
-                logger.log(Level.SEVERE, null, ex);
-            }
-            return result;
-        });
-        
+        return Db.singleString(()-> doAdd(element));
+    }
+    
+    public String doAdd(Closeable element) {
+        String result = "";
+        try {
+            result = tryExecuteAdd(element);
+        } catch (SQLException ex) {
+            log.error("Error adding closeable", ex);
+        }
+        return result;
+    }
+    
+    private String tryExecuteAdd(Closeable element) throws SQLException {
+        PreparedStatement preparedStatement = buildAddQuery(element);
+        preparedStatement.executeUpdate();
+        return element.getProcess();
+    }
+    
+    private PreparedStatement buildAddQuery(Closeable element) throws SQLException {
+        PreparedStatement preparedStatement = dbInstance.prepareStatement(String.format("INSERT INTO %s (%s) ",
+                Db.CLOSEABLES_TABLE, Closeable.PROCESS_NAME)
+                + "VALUES (?)");
+        preparedStatement.setString(1, element.getProcess());
+        return preparedStatement;
     }
 
     @Deprecated
     @Override
     public Single<Long> update(Closeable element) {
-        // single column table, nothing to update
-        return Single.just(0L);
+        throw new RuntimeException("Unsupported operation");
     }
 
     @Override
@@ -71,7 +69,7 @@ public class CloseableRepository implements CloseableDao {
                         Db.CLOSEABLES_TABLE));
                 rs = stm.executeQuery();
             } catch (SQLException ex) {
-                logger.log(Level.SEVERE, null ,ex);
+                log.error("Error finding all closeables", ex);
             }
             return rs;
         });
@@ -79,19 +77,30 @@ public class CloseableRepository implements CloseableDao {
 
     @Override
     public Single<ResultSet> findById(String id) {
-        return Db.singleResultSet(() -> {
-            ResultSet rs = null;
-            PreparedStatement stm;
-            try {
-                stm = dbInstance.getConnection().prepareStatement(String.format("SELECT * FROM %s WHERE %s=?",
-                        Db.CLOSEABLES_TABLE, Closeable.PROCESS_NAME));
-                stm.setString(1, id);
-                rs = stm.executeQuery();
-            } catch (SQLException ex) {
-                logger.log(Level.SEVERE, null, ex);
-            }
-            return rs;
-        });
+        return Db.singleResultSet(() -> doFindById(id));
+    }
+    
+    private ResultSet doFindById(String id) {
+        ResultSet result = null;
+        try {
+            result = tryFindById(id);
+        } catch (SQLException ex) {
+            log.error("Error finding by Id", ex);
+        }
+        return result;
+    }
+    
+    private ResultSet tryFindById(String id) throws SQLException {
+        PreparedStatement preparedStatement = buildFindByIdQuery(id);
+        return preparedStatement.executeQuery();
+    }
+    
+    private PreparedStatement buildFindByIdQuery(String id) throws SQLException {
+        PreparedStatement preparedStatement;
+        preparedStatement = dbInstance.prepareStatement(String.format("SELECT * FROM %s WHERE %s=?",
+                Db.CLOSEABLES_TABLE, Closeable.PROCESS_NAME));
+        preparedStatement.setString(1, id);
+        return preparedStatement;
     }
 
     @Override
@@ -105,7 +114,7 @@ public class CloseableRepository implements CloseableDao {
                 stm.setString(1, id);
                 delQty = stm.executeUpdate();
             } catch (SQLException ex) {
-                logger.log(Level.SEVERE, null, ex);
+                log.error("Error deleting by id", ex);
             }
             return delQty;
         });
