bd3de4b3c1c08ae59536c4c4759bb1f977f2d289
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-01-06 01:01:39 +0100

    testing

---
+++ src/test/java/devs/mrp/turkeydesktop/database/config/ConfigElementServiceTest.java
@@ -0,0 +1,13 @@
+package devs.mrp.turkeydesktop.database.config;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class ConfigElementServiceTest {
+
+    @Test
+    public void testSomeMethod() {
+        fail();
+    }
+
+}
