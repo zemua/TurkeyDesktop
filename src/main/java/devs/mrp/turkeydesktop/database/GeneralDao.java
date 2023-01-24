5424de504c7bf6a19b0320c278dcdeffbb458d78
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-01-24 21:33:11 +0100

    remove useless comment

--- src/main/java/devs/mrp/turkeydesktop/database/GeneralDao.java
+++ src/main/java/devs/mrp/turkeydesktop/database/GeneralDao.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package devs.mrp.turkeydesktop.database;
 
 import java.sql.ResultSet;
 import io.reactivex.rxjava3.core.Single;
 
-/**
- *
- * @author miguel
- */
 public interface GeneralDao<VALUE, KEY> {
     
     public Single<KEY> add(VALUE element);
