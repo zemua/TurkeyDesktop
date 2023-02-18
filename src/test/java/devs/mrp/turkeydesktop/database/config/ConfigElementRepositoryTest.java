42bc3b99050b7a8252c99157e711d3739d13729e
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-02-18 23:01:15 +0100

    injecting factory instead of static factory

--- src/test/java/devs/mrp/turkeydesktop/database/config/ConfigElementRepositoryTest.java
+++ src/test/java/devs/mrp/turkeydesktop/database/config/ConfigElementRepositoryTest.java
@@ -5,9 +5,11 @@ import devs.mrp.turkeydesktop.view.configuration.ConfigurationEnum;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.Callable;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -41,39 +43,49 @@ public class ConfigElementRepositoryTest {
     }
 
     @Test
-    public void testFindByIdReturnsResultSet() throws SQLException {
+    public void testFindByIdReturnsResultSet() throws SQLException, Exception {
         PreparedStatement preparedStatement = mock(PreparedStatement.class);
         when(db.prepareStatement(ArgumentMatchers.contains("SELECT * FROM CONFIG_TABLE WHERE KEY=?")))
                 .thenReturn(preparedStatement);
         ResultSet configResult = mock(ResultSet.class);
         when(preparedStatement.executeQuery()).thenReturn(configResult);
         
-        ResultSet resultSet = configRepository.findById(configElement.getKey().toString()).blockingGet();
+        ArgumentCaptor<Callable<ResultSet>> captor = ArgumentCaptor.forClass(Callable.class);
+        configRepository.findById(configElement.getKey().toString());
+        verify(db).singleResultSet(captor.capture());
+        ResultSet resultSet = captor.getValue().call();
+        
         assertEquals(configResult, resultSet);
         verify(preparedStatement, times(1)).setString(1, configElement.getKey().toString());
     }
     
     @Test
-    public void testAddSuccess() throws SQLException {
+    public void testAddSuccess() throws SQLException, Exception {
         PreparedStatement preparedStatement = mock(PreparedStatement.class);
         when(db.prepareStatementWithGeneratedKeys(ArgumentMatchers.contains("INSERT INTO CONFIG_TABLE (KEY, VALUE) VALUES (?, ?)")))
                 .thenReturn(preparedStatement);
         
-        String result = configRepository.add(configElement).blockingGet();
-        assertEquals(configElement.getKey().toString(), result);
+        ArgumentCaptor<Callable> captor = ArgumentCaptor.forClass(Callable.class);
+        configRepository.add(configElement);
+        verify(db).singleString(captor.capture());
+        
+        assertEquals(configElement.getKey().toString(), captor.getValue().call());
         verify(preparedStatement, times(1)).setString(1, configElement.getKey().toString());
         verify(preparedStatement, times(1)).setString(2, configElement.getValue());
     }
     
     @Test
-    public void testAddException() throws SQLException {
+    public void testAddException() throws SQLException, Exception {
         PreparedStatement preparedStatement = mock(PreparedStatement.class);
         when(db.prepareStatementWithGeneratedKeys(ArgumentMatchers.contains("INSERT INTO CONFIG_TABLE (KEY, VALUE) VALUES (?, ?)")))
                 .thenReturn(preparedStatement);
         when(preparedStatement.executeUpdate()).thenThrow(new SQLException());
+        ArgumentCaptor<Callable> captor = ArgumentCaptor.forClass(Callable.class);
         
-        String result = configRepository.add(configElement).blockingGet();
-        assertEquals("", result);
+        configRepository.add(configElement);
+        verify(db).singleString(captor.capture());
+        
+        assertEquals("", captor.getValue().call());
         verify(preparedStatement, times(1)).setString(1, configElement.getKey().toString());
         verify(preparedStatement, times(1)).setString(2, configElement.getValue());
     }
