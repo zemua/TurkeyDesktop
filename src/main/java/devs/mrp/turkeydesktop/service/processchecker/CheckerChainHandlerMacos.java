12234fa494214a6125cf8347f9b416b11391e094
ncm55070 <ncm55070@192.168.0.25>
2023-12-08 22:29:03 +0100

    update for sonoma to get pid and process name

--- src/main/java/devs/mrp/turkeydesktop/service/processchecker/CheckerChainHandlerMacos.java
+++ src/main/java/devs/mrp/turkeydesktop/service/processchecker/CheckerChainHandlerMacos.java
@@ -21,51 +21,31 @@ public class CheckerChainHandlerMacos extends ChainHandler<ProcessInfo> {
     @Override
     protected void handle(ProcessInfo data) {
         try {
-            String frontAppQuery = "lsappinfo front";
-            String frontApp = getDirectOutput(frontAppQuery);
-
-            String pidQuery = "lsappinfo info -only pid ".concat(frontApp);
-            String nameQuery = "lsappinfo info -only name ".concat(frontApp);
-            
-            String[] queryForTitle = {"osascript", "-e", "tell application \"System Events\" to tell (process 1 whose it is frontmost) ¬\n"
-                + "        to tell (window 1 whose value of attribute \"AXMain\" is true) ¬\n"
-                + "        to set windowTitle to value of attribute \"AXTitle\""};
-
-            data.setProcessName(getOneLinedValueOf(nameQuery));
-            data.setProcessPid(getOneLinedValueOf(pidQuery));
-            data.setWindowTitle(getOneLinedAppleScript(queryForTitle));
+            data.setProcessName(getOneLinedAppleScript(queryForProcess("name")));
+            data.setProcessPid(getOneLinedAppleScript(queryForProcess("unix id")));
+            data.setWindowTitle(getOneLinedAppleScript(queryForWindowTitle()));
         } catch (IOException ex) {
             Logger.getLogger(CheckerChainHandlerMacos.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
             Logger.getLogger(CheckerChainHandlerMacos.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    private String getOneLinedValueOf(String query) throws IOException, InterruptedException {
-        Process p = r.exec(query);
-        p.waitFor();
-        BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line = b.readLine();
-        b.close();
-        return Optional.ofNullable(line)
-                .map(l -> line.split("="))
-                .filter(arr -> arr.length == 2)
-                .map(arr -> arr[1])
-                .map(str -> str.replace("\"", ""))
-                .orElse("");
+    
+    private String[] queryForProcess(String property) {
+        String[] queryForProcess = {"osascript", "-e", 
+            "tell application \"System Events\" to tell (first process whose frontmost is true) to return {" + property + "}"};
+        return queryForProcess;
+    }
+    
+    private String[] queryForWindowTitle() {
+        String[] queryForTitle = {"osascript", "-e", "tell application \"System Events\" to tell (process 1 whose it is frontmost) ¬\n"
+                + "        to tell (window 1 whose value of attribute \"AXMain\" is true) ¬\n"
+                + "        to set windowTitle to value of attribute \"AXTitle\""};
+        return queryForTitle;
     }
 
     private String getOneLinedAppleScript(String[] comands) throws IOException, InterruptedException {
         Process p = r.exec(comands);
-        p.waitFor();
-        BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line = Optional.ofNullable(b.readLine()).orElse("");
-        b.close();
-        return line;
-    }
-
-    private String getDirectOutput(String query) throws IOException, InterruptedException {
-        Process p = r.exec(query);
         p.waitFor();
         BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line = Optional.ofNullable(b.readLine()).orElse("");
